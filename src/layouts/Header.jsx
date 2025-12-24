7c1ce7bf69f7b61997c3d009d4afc02fd5fddf74
choidonghun <cdh3946@gmail.com>
2025-12-24 18:02:45 +0900

    STYLE : Modal, Header, Button css 수정

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -8,27 +8,39 @@ import Button from 'components/Button';
 const Header = (theme) => {
   const { openModal } = useModal();
   return (
-    <header className="header">
-      <div className="wrapper" data-theme={theme="black"}>
+    <header className="PL-Nav" data-theme={theme="black"}>
+      <div className="wrapper">
+        <div className="title">
+          <Link to="/" className="link">
+            <img src={PlayLogLogo} alt="playlog-icon" />
+          </Link>
+          <Link to="/" className="link app-name">
+            PlayLog
+          </Link>
+        </div>
+        <div className="content">
+          <div className="search">
+            <form class="search-bar" onSubmit={(e)=>{
+              e.preventDefault();
+              //API 요청
+            }}>
+              <input className="search-input" type="text" placeholder='Search'/>
+            </form>
+          </div>
+
+          <div className="nav">
+            <Link to="/games/new" className="link">신작 게임</Link>
+            <Link to="/games/popular" className="link">인기 게임</Link>
+            <Button className="link" type="header" handleClick={()=>{openModal('Login');}}>로그인</Button>
+          </div>
+
+          <div className="setting">
+
+          </div>
+        </div>
+       
         
-        <Link to="/" className="link">
-          <img src={PlayLogLogo} alt="playlog-icon" />
-        </Link>
-        <Link to="/" className="link app-name">
-          PlayLog
-        </Link>
-        <div className="search">
-
-        </div>
-        <nav className="nav">
-          <Link to="/games/new" className="link">신작 게임</Link>
-          <Link to="/games/popular" className="link">인기 게임</Link>
-          <Button className="link" type="header" handleClick={()=>{openModal('Login');}}>로그인</Button>
-        </nav>
-
-        <div className="setting">
-
-        </div>
+        
       </div>
     </header>
   );
